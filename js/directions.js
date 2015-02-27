42d3c40750e9e4a3967eccc8546ec742cc7ace90
madsailor <drbluesman@yahoo.com>
2015-02-27 15:53:24 -0800

    Add content to purchase section

--- js/directions.js
+++ js/directions.js
@@ -4,12 +4,13 @@ var directionsService = new google.maps.DirectionsService();
 function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
   var mapOptions = {
+    scrollwheel: false,
     zoom: 15,
     center: new google.maps.LatLng(33.756, -118.19287)
   };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  map.setMapTypeId(google.maps.MapTypeId.HYBRID);
+  map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById('directions-panel'));
   var marker = new google.maps.Marker({
